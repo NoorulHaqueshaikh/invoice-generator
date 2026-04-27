fb6e91d7b1ea531968fd839898e9efafbffad45a
NoorulHaqueshaikh <noorulhaque560@gmail.com>
2026-04-27 21:40:32 +0530

    sitemap added

---
+++ src/app/sitemap.js
@@ -0,0 +1,12 @@
+// app/sitemap.js
+
+export default function sitemap() {
+  return [
+    {
+      url: "https://invoice.andicode.com",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+  ];
+}
