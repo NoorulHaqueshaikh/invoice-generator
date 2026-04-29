5d7ae87c2b0ee821c6597e1770cfb7ce4529d312
NoorulHaqueshaikh <noorulhaque560@gmail.com>
2026-04-29 18:18:17 +0530

    c

--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -39,7 +39,7 @@ function Navbar() {
           {/* Desktop Menu - About Us */}
           <div className="hidden md:flex items-center">
             <a 
-              href="/about" 
+              href="/about-us" 
               className="text-white hover:text-[#111827] hover:bg-[#DBEAFE] px-4 py-2 rounded-lg text-sm font-semibold transition-colors"
             >
               About Us
